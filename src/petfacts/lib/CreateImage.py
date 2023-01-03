ca5883f584e97118b7b668488f9e9fff3eb0aeb4
CodeCanna <codecannamw@gmail.com>
2023-01-02 17:42:24 -0700

    Added Pillow

--- src/petfacts/lib/CreateImage.py
+++ src/petfacts/lib/CreateImage.py
@@ -1,8 +1,8 @@
+import PIL
+
 class CreateImage:
     def __init__(self, animal_data: AnimalGetter):
         self.animal_getter = animal_data
     
-    # Returns the path of the created image
-    @classmethod
-    def image(self) -> str:
+    def create(self, text: str, image: Path):
         pass
