e2f6d4f7ae31e6044f1dfebd334cd69c85efe546
CodeCanna <codecannamw@gmail.com>
2023-01-27 00:22:00 -0700

    Added TextModifier class.

--- src/petfacts/lib/TextModifier.py
+++ src/petfacts/lib/TextModifier.py
@@ -4,5 +4,6 @@ class TextModifier(textwrap.TextWrapper):
     def __init__(self, max_width: int) -> None:
         super().__init__(width=max_width)
 
+    # Sets the max width of text written to an image before next line
     def modify(self, text: str):
         return self.wrap(text)
