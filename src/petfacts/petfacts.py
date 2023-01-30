1afa09397512281de17a9487f205b094c18d27b3
CodeCanna <codecannamw@gmail.com>
2023-01-29 20:43:26 -0700

    Removed un-needed code.

--- src/petfacts/petfacts.py
+++ src/petfacts/petfacts.py
@@ -9,55 +9,25 @@ import configparser
 import os
 
 from pathlib import *
-from sys import platform  # For detecting the running os
+from sys import platform  # For detecting the running os (Linux, Windows, or Mac)
 from lib.AnimalGetter import AnimalGetter
 from lib.Parser import Parser
 from lib.CreateImage import CreateImage
 
 """
-This is a simple little project to help me learn argparse, an argument parser like getopt but has a few advantages.
-Hopefully those advantages will be clear soon.
+Pet facts started off as a simple project to teach myself argparse on Python, but it kind of turned into a larger project.
+This project has grown into a pet fact image generator.  It gets an image of a dog or cat, gets a random fact about it; then prints it out on top
+of the image.
+
+It saves the image to the disk for viewing or sharing.
 """
 
-# Get a random cat fact and return it as a string
-
-
-def cat_fact() -> str:
-    rsession = requests.session()
-    try:
-        fact_json = rsession.get('https://catfact.ninja/fact')
-        fact = json.loads(fact_json.text)
-    except json.JSONDecodeError as err:
-        print(f"Couldn't show the cat fact because {err}")
-    except requests.HTTPError as err:
-        print(f"Couldn't get the cat fact due to this HTTP error: {err}")
-
-    return fact['fact']
-
-# Get a random dog fact and return it as a string
-
-
-def dog_fact() -> str:
-    rsession = requests.session()
-    try:
-        fact_json = rsession.get(
-            'http://dog-api.kinduff.com/api/facts?number=1')
-        fact = json.loads(fact_json.text)
-    except json.JSONDecodeError as err:
-        print(f"Invalid repsonse recieved, got {err}")
-
-    return fact['facts'][0]
-
-# Choose a dog or cat string in an array and return it
-
-
+# Return the string 'dog' or 'cat' randomly.
 def random_animal() -> str:
     animals = ['dog', 'cat']
     return animals[random.randrange(len(animals))]
 
 # Add a delay to print each character for a nice effect
-
-
 def type(string: str) -> None:
     # loop through the string characters and print them one at a time.
     for character in string:
@@ -69,6 +39,7 @@ def type(string: str) -> None:
 def run_on_linux(config: configparser.ConfigParser, animal_data: AnimalGetter):
     config.read('config.ini')
 
+    # Parse our linux paths
     tmp_save_path: str = str(PurePosixPath(config['tmp_paths']['linux_tmp_path']))
     save_path: str = str(PurePosixPath(config['paths']['linux_path']))
 
@@ -86,7 +57,6 @@ def run_on_linux(config: configparser.ConfigParser, animal_data: AnimalGetter):
     if not os.path.exists(tmp_save_path_parsed):
         print(f"{tmp_save_path_parsed} not found creating...")
         os.makedirs(tmp_save_path_parsed)
-
     try:
         img_bytes = requests.get(animal_data['image']).content
         with open(tmp_save_path_parsed + "/tmp_img" + Parser.get_extension(animal_data['image']), 'wb') as img:
@@ -101,7 +71,6 @@ def run_on_linux(config: configparser.ConfigParser, animal_data: AnimalGetter):
         print(f"Couldn't find or create directory: {err}")
     except OSError as err:
         print("Couldn't write file possibly due to wrong permissions.")
-
 
 # This function runs if the program is being ran on windows
 # This function parses and saves the image and fact data based on the windows file system stucture and path.
@@ -139,7 +108,10 @@ def run_on_win32(config: configparser.ConfigParser, animal_data: dict):
 
 
 def main():
+    # Get the config file
     config: configparser.ConfigParser = configparser.ConfigParser()
+
+    # Set up our argparse object for processing arguments.
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
         prog='Petfacts',
         description='A pet fact and pet image generator.',
@@ -157,14 +129,33 @@ def main():
     if platform == 'win32':
         config_path = Path('.\\config.ini')
         request_session = requests.session()
-        if args.noimage:
-            animal_getter = AnimalGetter(random_animal(), request_session, config_path, False)
+        if args.noimage and args.cat: # Specify no image cat fact
+            animal_getter = AnimalGetter('cat', request_session, config_path, False)
             animal_data = animal_getter.get(config)
 
             fact = animal_data['fact']
             type(fact)
             exit(0)
-        else:   
+        elif args.noimage and args.dog: # Sepcify no image dog fact
+            animal_getter = AnimalGetter('dog', request_session, config_path, False)
+            animal_data = animal_getter.get(config)
+
+            fact = animal_data['fact']
+            type(fact)
+            exit(0)
+        elif args.cat:  
+            animal_getter = AnimalGetter('cat', request_session, config_path, True)
+            animal_data = animal_getter.get(config)
+
+            # Run this program for windows, handle the different file paths.
+            run_on_win32(config, animal_data)
+        elif args.dog:
+            animal_getter = AnimalGetter('dog', request_session, config_path, True)
+            animal_data = animal_getter.get(config)
+
+            # Run this program for windows, handle the different file paths.
+            run_on_win32(config, animal_data)
+        else:
             animal_getter = AnimalGetter(random_animal(), request_session, config_path, True)
             animal_data = animal_getter.get(config)
 
@@ -174,19 +165,42 @@ def main():
         config_path = Path('./config.ini')
         request_session = requests.session()
 
-        if args.noimage:
-            animal_getter = AnimalGetter(random_animal(), request_session, config_path, False)
+        if args.noimage and args.cat:
+            animal_getter = AnimalGetter('cat', request_session, config_path, False)
             animal_data = animal_getter.get(config)
 
             fact = animal_data['fact']
             type(fact)
             exit(0)
+        elif args.noimage and args.dog:
+            animal_getter = AnimalGetter('dog', request_session, config_path, False)
+            animal_data = animal_getter.get(config)
+
+            # Get the actual fact we are going to print from the returned dict from AnimalGetter.get()
+            fact = animal_data['fact']
+
+            # Display the fact, but "type" it out.
+            type(fact)
+
+            # Exit after printing the fact
+            exit(0)
+        elif args.cat:
+            animal_getter = AnimalGetter('cat', request_session, config_path, True)
+            animal_data = animal_getter.get(config)
+
+            # This funciton is called if this is ran on linux.
+            run_on_linux(config, animal_data)
+        elif args.dog:
+            animal_getter = AnimalGetter('dog', request_session, config_path, True)
+            animal_data = animal_getter.get(config)
+
+            # This function is called if this is ran on linux.
+            run_on_linux(config, animal_data)
         else:
             animal_getter = AnimalGetter(random_animal(), request_session, config_path, True)
             animal_data = animal_getter.get(config)
 
             run_on_linux(config, animal_data)
-            print("Linux")
     elif platform == 'darwin':
         print("Mac")
 
