a01df721fcef037c142a6a9e6cf1ac3e29e21e69
CodeCanna <codecannamw@gmail.com>
2023-01-31 23:51:59 -0700

    Fixxed linter complaints.

--- src/petfacts/petfacts.py
+++ src/petfacts/petfacts.py
@@ -8,7 +8,7 @@ import time
 import configparser
 import os
 
-from pathlib import *
+from pathlib import Path, PurePosixPath, PureWindowsPath
 from sys import platform  # For detecting the running os (Linux, Windows, or Mac)
 from lib.AnimalGetter import AnimalGetter
 from lib.Parser import Parser
@@ -36,7 +36,7 @@ def type(string: str) -> None:
         time.sleep(0.1)
 
 # This function runs if petfacts is being ran on a Linux machine.
-def run_on_linux(config: configparser.ConfigParser, animal_data: AnimalGetter):
+def run_on_linux(config: configparser.ConfigParser, animal_data: dict[str, str]):
     config.read('config.ini')
 
     # Parse our linux paths
@@ -74,7 +74,7 @@ def run_on_linux(config: configparser.ConfigParser, animal_data: AnimalGetter):
 
 # This function runs if the program is being ran on windows
 # This function parses and saves the image and fact data based on the windows file system stucture and path.
-def run_on_win32(config: configparser.ConfigParser, animal_data: dict):
+def run_on_win32(config: configparser.ConfigParser, animal_data: dict[str, str], filename: str):
     config.read('config.ini')  # Read the config file
     # Get our windows path parsed and ready...oh Windows...
     tmp_save_path = str(PureWindowsPath(config['tmp_paths']['win_tmp_path']))
@@ -98,13 +98,11 @@ def run_on_win32(config: configparser.ConfigParser, animal_data: dict):
         img = CreateImage.create(
             animal_data['fact'], f"{tmp_save_path_parsed}\\tmp_img{Parser.get_extension(animal_data['image'])}", 15, 15)
         CreateImage.save(
-            f"{Parser.get_win_path(save_path_parsed)}\\image{Parser.get_extension(animal_data['image'])}", img)
+            f"{Parser.get_win_path(save_path_parsed)}\\{filename}{Parser.get_extension(animal_data['image'])}", img)
     except FileNotFoundError as err:
         print(f"Couldn't find or create dir: {err}")
     except OSError as err:
-        print(
-            f"Couldn't write to path {Parser.get_win_path(tmp_save_path)}: {err}")
-        print("Make sure you have proper permissions to write to this file, on windows this script might have to be ran as Administrator!")
+        print(f"Couldn't write to file, file either already exists or you do not have permissions to write files.")
 
 
 def main():
@@ -122,7 +120,9 @@ def main():
     parser.add_argument('--dog', action='store_true', help='Get a random dog fact with or without image.')
     parser.add_argument('--cat', action='store_true', help='Get a random cat fact with or without image.')
     parser.add_argument('--noimage', action='store_true', help='Specify that you just want a fact with no image.')
+    parser.add_argument("-s", "--saveas", help='Give the name of the image to save under without file extension.')
 
+    # Parse our args
     args = parser.parse_args()
 
     # Check our platform; is it Linux, Mac, or Windows?  I'm trying to account for different file structures on different systems.
@@ -135,6 +135,8 @@ def main():
 
             fact = animal_data['fact']
             type(fact)
+
+            # Exit after typing the fact to the terminal.
             exit(0)
         elif args.noimage and args.dog: # Sepcify no image dog fact
             animal_getter = AnimalGetter('dog', request_session, config_path, False)
@@ -142,25 +144,26 @@ def main():
 
             fact = animal_data['fact']
             type(fact)
+            # Exit after typing the fact to the terminal.
             exit(0)
         elif args.cat:  
             animal_getter = AnimalGetter('cat', request_session, config_path, True)
             animal_data = animal_getter.get(config)
 
             # Run this program for windows, handle the different file paths.
-            run_on_win32(config, animal_data)
+            run_on_win32(config, animal_data, args.saveas)
         elif args.dog:
             animal_getter = AnimalGetter('dog', request_session, config_path, True)
             animal_data = animal_getter.get(config)
 
             # Run this program for windows, handle the different file paths.
-            run_on_win32(config, animal_data)
+            run_on_win32(config, animal_data, args.saveas)
         else:
             animal_getter = AnimalGetter(random_animal(), request_session, config_path, True)
             animal_data = animal_getter.get(config)
 
             # Run this program for windows, handle the different file paths.
-            run_on_win32(config, animal_data)
+            run_on_win32(config, animal_data, args.saveas)
     elif platform == 'linux' or 'linux2':
         config_path = Path('./config.ini')
         request_session = requests.session()
@@ -171,6 +174,8 @@ def main():
 
             fact = animal_data['fact']
             type(fact)
+
+            # Exit after typing the fact to the terminal.
             exit(0)
         elif args.noimage and args.dog:
             animal_getter = AnimalGetter('dog', request_session, config_path, False)
@@ -185,20 +190,20 @@ def main():
             # Exit after printing the fact
             exit(0)
         elif args.cat:
-            animal_getter = AnimalGetter('cat', request_session, config_path, True)
-            animal_data = animal_getter.get(config)
+            animal_getter: AnimalGetter = AnimalGetter('cat', request_session, config_path, True)
+            animal_data: dict[str, str] = animal_getter.get(config)
 
             # This funciton is called if this is ran on linux.
             run_on_linux(config, animal_data)
         elif args.dog:
-            animal_getter = AnimalGetter('dog', request_session, config_path, True)
-            animal_data = animal_getter.get(config)
+            animal_getter: AnimalGetter = AnimalGetter('dog', request_session, config_path, True)
+            animal_data: dict[str, str] = animal_getter.get(config)
 
             # This function is called if this is ran on linux.
             run_on_linux(config, animal_data)
         else:
-            animal_getter = AnimalGetter(random_animal(), request_session, config_path, True)
-            animal_data = animal_getter.get(config)
+            animal_getter: AnimalGetter = AnimalGetter(random_animal(), request_session, config_path, True)
+            animal_data: dict[str, str] = animal_getter.get(config)
 
             run_on_linux(config, animal_data)
     elif platform == 'darwin':
