6d191cbd0f5470e737760bea917fbfdc9f2342ef
CodeCanna <codecannamw@gmail.com>
2023-01-31 23:53:04 -0700

    Fixxed error handling with duplicate files.

--- src/petfacts/lib/CreateImage.py
+++ src/petfacts/lib/CreateImage.py
@@ -1,16 +1,17 @@
 from .TextModifier import TextModifier
 from PIL import Image, ImageDraw, ImageFont
-from pathlib import *
+from pathlib import Path
+import os
 
 class CreateImage:    
     """
     text = Text to print to the image
     image = path to the tmp image to edit
-    xp = The percentage desired from image width
-    yp = The percentage desired from image height
+    x = The x coordinate of the text printed to image
+    y = The y coordinate of the text printed to the image
     """
     @classmethod
-    def create(cls, text: str, image: str, x: int, y: int):# -> Image:
+    def create(cls, text: str, image: str, x: int, y: int) -> Image.Image:
         with Image.open(image) as img:
             image_editted = ImageDraw.Draw(img)
             modifier = TextModifier(30) # To wrap out text on the image
@@ -18,13 +19,16 @@ class CreateImage:
             font = ImageFont.truetype('fonts/sans_rounded.ttf', size=30)
             for line in text_array: # Loop throught the array of strings returned from TextModifier.modify()
                 y = y + 35 # Add the size of the font plus five to Y for each line.
-                image_editted.text((x, y), line, fill=(255, 255, 255), font=font)
+                image_editted.text((x, y), line, fill=(128, 128, 128), font=font)
             return img
 
     @classmethod
-    def save(cls, path: str, image_to_save: Image.Image) -> None:
+    def save(cls, _path: str, image_to_save: Image.Image) -> None:
         try:
-            print(path)
-            image_to_save.save(path)
-        except FileNotFoundError as err:
-            print(f"Couldn't find file or path: {err}")
+            # If the filename is already found raise an error.
+            if os.path.exists(_path):
+                raise(FileExistsError(f"File {_path} already exists.  Please choose another file name."))
+            else:
+                image_to_save.save(Path(_path))
+        except FileExistsError as err:
+            print(err)
