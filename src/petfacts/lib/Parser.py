92de0bef6f77fb974390c1b07ed463f903985cdd
CodeCanna <codecannamw@gmail.com>
2023-01-31 23:53:31 -0700

    Changed classmethod selfs to cls.

--- src/petfacts/lib/Parser.py
+++ src/petfacts/lib/Parser.py
@@ -2,19 +2,19 @@ import os, getpass
 
 class Parser:
     @classmethod
-    def get_win_path(self, path: str) -> str:
+    def get_win_path(cls, path: str) -> str:
         return path.replace('<USERNAME>', os.getlogin()).strip()
 
     @classmethod
-    def get_linux_path(self, path: str) -> str:
+    def get_linux_path(cls, path: str) -> str:
         #return path.replace('$USER', os.getlogin()).strip()
         return path.replace('$USER', getpass.getuser()).strip()
 
     @classmethod
-    def get_mac_path(self, path: str) -> str:
+    def get_mac_path(cls, path: str) -> str:
         return path.replace('$USER', os.getlogin()).strip()
 
     @classmethod
-    def get_extension(self, url: str) -> str:
+    def get_extension(cls, url: str) -> str:
         (root, extension) = os.path.splitext(url)
         return extension
